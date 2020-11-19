9e643e4301c8efbbf51a1909b9e40e02f29a8271
Igor Gorbunov <give@list.ru>
2020-11-19 13:07:16 +0400

    add DTO model Drill

--- Models/DTO/DrillDto.cs
+++ Models/DTO/DrillDto.cs
@@ -1,40 +1,41 @@
-﻿using System;
+﻿using DrillShopApi.DAL.Domain;
+using System.ComponentModel.DataAnnotations;
 
-namespace DrillShopApi.DAL.Domain
+namespace DrillShopApi.Models.DTO
 {
     /// <summary>
-    /// Сверло
+    /// DTO для <see cref="Drill"/>
     /// </summary>
-    public class Drill
+    public class DrillDto
     {
-        /// <summary>
-        /// Идентификатор записи
-        /// </summary>
-        public long Id { get; set; }
-
         /// <summary>
         /// Артикул
         /// </summary>
+        [Required]
         public string ArtCode { get; set; }
 
         /// <summary>
         /// Описание
         /// </summary>
+        [MaxLength(2000)]
         public string Description { get; set; }
-        
+
         /// <summary>
         /// Вес
         /// </summary>
+        [Required]
         public double Weight { get; set; }
 
         /// <summary>
         /// Минимальный диаметр
         /// </summary>
+        [Required]
         public double MinDiametr { get; set; }
 
         /// <summary>
         /// Максимальный диаметр
         /// </summary>
+        [Required]
         public double MaxDiametr { get; set; }
     }
 }

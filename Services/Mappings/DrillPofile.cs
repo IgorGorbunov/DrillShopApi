8059479c4e56353cd01b6708f93a43c31f90803f
Igor Gorbunov <give@list.ru>
2020-11-19 13:20:09 +0400

    add Drill mapping

---
+++ Services/Mappings/DrillPofile.cs
@@ -0,0 +1,20 @@
+﻿using DrillShopApi.DAL.Domain;
+using DrillShopApi.Models.DTO;
+using AutoMapper;
+
+namespace Services.Mappings
+{
+    /// <summary>
+    /// Профиль маппинга (сверла)
+    /// </summary>
+    public class DrillPofile : Profile
+    {
+        /// <summary>
+        /// Инициализирует экземпляр <see cref="DrillPofile"/>
+        /// </summary>
+        public DrillPofile()
+        {
+            CreateMap<Drill, DrillDto>();
+        }
+    }
+}

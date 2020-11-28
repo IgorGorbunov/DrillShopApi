ddeeacbf569e47611d7019796015436fd81155e1
Igor Gorbunov <give@list.ru>
2020-11-28 22:32:49 +0400

    add base domain models

---
+++ DAL/Domain/BaseStructEntity.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DrillShopApi.DAL.Domain
+{
+    /// <summary>
+    /// Базовый класс для структурных доменных моделей.
+    /// </summary>
+    public abstract class BaseStructEntity : BaseEntity
+    {
+        /// <summary>
+        /// Атрибут удаления записи.
+        /// </summary>
+        [Required]
+        public bool IsDeleted { get; set; }
+
+        /// <summary>
+        /// Дата создания записи.
+        /// </summary>
+        [Required]
+        public DateTime Created { get; set; }
+
+        /// <summary>
+        /// Дата изменения записи.
+        /// </summary>
+        public DateTime Modified { get; set; }
+
+        /// <summary>
+        /// Дата удаления записи.
+        /// </summary>
+        public DateTime Deleted { get; set; }
+    }
+}

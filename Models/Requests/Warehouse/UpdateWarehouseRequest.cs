b309ca0d11f8672127edf2ea995baed748bd71c3
Igor Gorbunov <give@list.ru>
2020-12-01 02:49:57 +0400

    add entity update requests

---
+++ Models/Requests/Warehouse/UpdateWarehouseRequest.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DrillShopApi.Models.Requests.Warehouse
+{
+    /// <summary>
+    /// Запрос на изменение позиции склада.
+    /// </summary>
+    public class UpdateWarehouseRequest : CreateWarehouseRequest
+    {
+        /// <summary>
+        /// Идентификатор сущности.
+        /// </summary>
+        [Required]
+        public long Id { get; set; }
+    }
+}

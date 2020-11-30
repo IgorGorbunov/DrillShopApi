e7f26410c07fb626b09382b92d671b59b6d21b88
Igor Gorbunov <give@list.ru>
2020-12-01 00:20:38 +0400

    add repo configuration, add CRUD interfaces, add IDrill repository

---
+++ DrillShopApi.Repositories/Interfaces/CRUD/IGettable.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DrillShopApi.Repositories.Interfaces.CRUD
+{
+    /// <summary>
+    /// Интерфейс для получения сущностей.
+    /// </summary>
+    /// <typeparam name="TDto">DTO.</typeparam>
+    /// <typeparam name="TModel">Domain model.</typeparam>
+    public interface IGettable<TDto, TModel>
+    {
+        /// <summary>
+        /// Получение сущностей.
+        /// </summary>
+        /// <param name="token">Экземпляр <see cref="CancellationToken"/>.</param>
+        /// <returns></returns>
+        Task<IEnumerable<TDto>> GetAsync(CancellationToken token = default);
+    }
+}

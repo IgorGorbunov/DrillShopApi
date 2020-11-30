be11cb6ee2acfb12581b131a59ac8369b1dcd0e1
Igor Gorbunov <give@list.ru>
2020-11-30 17:42:59 +0400

    add db context, db configuration

---
+++ DAL/Contexts/DrillShopContext.cs
@@ -0,0 +1,51 @@
+﻿using DrillShopApi.DAL.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace DrillShopApi.DAL.Contexts
+{
+    /// <summary>
+    /// Контекст для работы с данными БД "Магазины сверл".
+    /// </summary>
+    public class DrillShopContext : DbContext
+    {
+        /// <summary>
+        /// Магазины.
+        /// </summary>
+        public DbSet<Shop> Shops { get; set; }
+
+        /// <summary>
+        /// Поставщики.
+        /// </summary>
+        public DbSet<Provider> Providers { get; set; }
+
+        /// <summary>
+        /// Склады.
+        /// </summary>
+        public DbSet<Warehouse> Warehouses { get; set; }
+
+        /// <summary>
+        /// Сверла.
+        /// </summary>
+        public DbSet<Drill> Drills { get; set; }
+
+        /// <summary>
+        /// Наличие в магазинах.
+        /// </summary>
+        public DbSet<ShopAvailability> ShopAvailabilities { get; set; }
+
+        /// <summary>
+        /// Наличие на складах.
+        /// </summary>
+        public DbSet<WHAvailability> WHAvailabilities { get; set; }
+
+        /// <summary>
+        /// Инициализирует экземпляр <see cref="DrillShopContext"/>.
+        /// </summary>
+        /// <param name="options">Опции для конфигурации контекста.</param>
+        public DrillShopContext(DbContextOptions options) : base(options)
+        {
+            Database.EnsureDeleted();
+            Database.EnsureCreated();
+        }
+    }
+}

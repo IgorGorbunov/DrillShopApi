7951d3716d9672c5302f1185669dc95485956f53
Igor Gorbunov <give@list.ru>
2020-12-01 03:01:51 +0400

    add entity responses

---
+++ Models/Responses/WarehouseResponse.cs
@@ -0,0 +1,33 @@
+﻿namespace DrillShopApi.Models.Responses
+{
+    /// <summary>
+    /// Ответ на запросы для позиций сверла.
+    /// </summary>
+    public class WarehouseResponse
+    {
+        /// <summary>
+        /// Наименование.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Город.
+        /// </summary>
+        public string City { get; set; }
+
+        /// <summary>
+        /// Адрес.
+        /// </summary>
+        public string Address { get; set; }
+
+        /// <summary>
+        /// Площадь помещения.
+        /// </summary>
+        public double Area { get; set; }
+
+        /// <summary>
+        /// Вместимость помещения.
+        /// </summary>
+        public double Capacity { get; set; }
+    }
+}

5a1ec4f1ca3911af571dd5ca8bd948503050662a
Igor Gorbunov <give@list.ru>
2020-12-01 02:02:35 +0400

    add crud and service interfaces

--- Services/Interfaces/IDrillService.cs
+++ Services/Interfaces/IDrillService.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿
 using DrillShopApi.Models.DTO;
 
 namespace DrillShopApi.Services.Interfaces
@@ -8,9 +6,9 @@ namespace DrillShopApi.Services.Interfaces
     /// <summary>
     /// Интерфейс сервиса для работы с данными о сверлах.
     /// </summary>
-    public interface IDrillService
+    public interface IDrillService : ICrudService<DrillDto>
     {
-        IEnumerable<DrillDto> Get();
+
     }
 
 }
